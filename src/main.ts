3d07aa6e0f35ef5cc118a320449f7f005560f318
Jeferson Delgado <92939552+jeferD@users.noreply.github.com>
2023-10-22 18:47:13 -0500

    . casa final

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,34 @@ import { connectToServer } from './socket-client.ts';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <h1> WEBSOCKET CLIENT  </h1>
-    <span>Offline</span>
+    <h2> WEBSOCKET CLIENT  </h2>
+    <input id="jwToken" placeholder= "Json Web Token"/>
+    <button id="btnConnect">Conectarse</button>
+    <br/>
 
+    <span id="server-status">Offline</span>
+
+    <ul id="clients-ul">
+
+    </ul>
+
+    <form id="message-form">
+      <input placeholder="mensaje" id="message-input"></input>
+    </form>
+
+    <h3>Mensajes</h3>
+    <ul id="messages-ul">
+    </ul>
     
  </div>
 `
-connectToServer();
 // http://localhost:3008/socket.io/socket.io.js
+
+// connectToServer();
+const jwToken= document.querySelector<HTMLInputElement>('#jwToken')!
+const btnConnect= document.querySelector<HTMLButtonElement>('#btnConnect')!
+
+btnConnect.addEventListener('click', () => {
+  if(jwToken.value.trim().length <= 0) return alert('Ingrese un token valido')
+  connectToServer(jwToken.value);
+})
--- src/socket-client.ts
+++ src/socket-client.ts
@@ -1,7 +1,77 @@
-import {Manager} from 'socket.io-client'
-export const connectToServer = () =>{
+import {Manager, Socket} from 'socket.io-client'
+
+let socket:Socket
+export const connectToServer = (token: string) =>{
     // http://localhost:3008/socket.io/socket.io.js
-    const manager = new Manager('http://localhost:3008/socket.io/socket.io.js');
-    const socket = manager.socket('/');
+    const manager = new Manager('http://localhost:3000/socket.io/socket.io.js', {
+        extraHeaders:{
+            authentication: token
+        }
+    });
+    socket?.removeAllListeners()
+
+    socket = manager.socket('/');
     console.log('socket: ', {socket});
+    addListeners()
+}
+
+const addListeners = () => {
+    const serverStatusLabel = document.querySelector('#server-status')!
+    const clientsUl = document.querySelector('#clients-ul')!
+
+    // socket.on//escuchar el servidor
+    // socket.emit//es emitir algo al servidor
+
+    const messageForm = document.querySelector<HTMLFormElement>('#message-form')!
+    const messageInput = document.querySelector<HTMLInputElement>('#message-input')!
+    const messageUl = document.querySelector<HTMLUListElement>('#messages-ul')!
+
+    socket.on('connect',()=>{//cuando se conectan al socket coloca esto
+        console.log('Conectado desde el addListener')
+        serverStatusLabel.innerHTML='conectado'
+    })
+
+    socket.on('disconnect',()=>{//cuando se des conectan al socket coloca esto
+        console.log('Des-conectado desde el addListener')
+        serverStatusLabel.innerHTML='desconectado'
+
+    })
+
+    socket.on('clients-updated', (cliens: string[])=> {//cuando hay una peticion con este identificador clients-updated
+        console.log('cliens: ', cliens);
+        let clientsHtml = ''
+        cliens.forEach(clientId =>{
+            clientsHtml +=`
+                <li>${clientId}</li>
+            `
+
+        })
+
+        clientsUl.innerHTML = clientsHtml
+    })
+
+    messageForm.addEventListener('submit', (event)=>{
+        event.preventDefault();
+        if(messageInput.value.trim().length <= 0){
+            return 
+        }
+        console.log({id: 'Yo', message: messageInput.value})
+        socket.emit('message-from-client', {id: 'Yo', message: messageInput.value})
+        messageInput.value = ''
+    })
+
+
+    socket.on('message-from-server', (payload: {fullName: string, message: string}) =>{
+        console.log(payload)
+        const newMessage =`
+            <li>
+                <strong> ${payload.fullName}: </strong>
+                <span> ${payload.message}</span>
+            </li>
+        `
+
+        const li = document.createElement('li')
+        li.innerHTML = newMessage;
+        messageUl.append(li)
+    })
 }
